1a8d1746d0dacad84f2c73ddb1dda794d87f1141
akari0624 <akari0624@hotmail.com>
2018-11-18 09:21:10 +0800

    typed redux action

--- src/examples/redux_callAPIThenGenerateList/actions/index.ts
+++ src/examples/redux_callAPIThenGenerateList/actions/index.ts
@@ -1,16 +1,19 @@
 // import {action} from 'typesafe-actions'
-import { Dispatch } from 'redux'
+import { Dispatch, ActionCreator, Action } from 'redux'
 import CONSTANTS from '../types'
 
 import axios from 'axios'
+import { ThunkAction } from 'redux-thunk';
 const tvmazeBaseURL : string = 'http://api.tvmaze.com/search/shows?q='
 
-// export interface IActionFetchTVMazeData {
-//   type: string,
-//   payload: string
-// }
+export interface IActionFetchTVMazeData {
+  type: string,
+  payload: any
+}
 
-export const acFetchTVMazeData = (searchText : string) => (dispatch: Dispatch) => {
+export const acFetchTVMazeData: ActionCreator<
+    ThunkAction<void, null, null, IActionFetchTVMazeData>
+  > = (searchText : string) => (dispatch: Dispatch<IActionFetchTVMazeData>) => {
 
   const pResult = axios.get(`${tvmazeBaseURL}${searchText}`)
 
