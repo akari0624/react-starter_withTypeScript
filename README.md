d7c20448541d4f2ad5cf608ee8dca45a909e4ad4
akari0624 <akari0624@hotmail.com>
2018-11-23 17:15:09 +0800

    add example links

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
-### Examples
-  
+## Examples
+- [see them in web from index page](https://akari0624.github.io/react-starter_withTypeScript/)
+  - counter [code](https://github.com/akari0624/react-starter_withTypeScript/tree/examples/src/examples/counter) 
+  - todo list(not completed) [code](https://github.com/akari0624/react-starter_withTypeScript/tree/examples/src/examples/todos)
+  - fetch an API, generate a list manage state without REDUX [code](https://github.com/akari0624/react-starter_withTypeScript/tree/examples/src/examples/callAPIThenGenerateList)
+  - fetch an API, generate a list, manage global app state with REDUX [code](https://github.com/akari0624/react-starter_withTypeScript/tree/examples/src/examples/redux_callAPIThenGenerateList)
--- src/mainpage/index.tsx
+++ src/mainpage/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import Styled from 'styled-components'
+import { Link } from 'react-router-dom'
 
 /** in Typescript, we need to write a interface(I feel more like a protocol) to the incoming props
  *  so tsc(TypeScript Compiler) can help us check the type in compile time
@@ -19,6 +20,21 @@ export default(props: Props) => {
   return (
     <MiddleDiv>
       <h3>Greeting from React with TypeScript + bundle in webpack</h3>
+      <h4>Examples :</h4>
+      <div>
+        <div>
+          <Link to="/counter">Counter</Link>
+        </div>
+        <div>
+          <Link to="/todos">todo list</Link>
+        </div>
+        <div>
+          <Link to="/tvmaze">fetch an API, generate a list manage state without REDUX</Link>
+        </div>
+        <div>
+          <Link to="/tvmaze_redux">fetch an API, generate a list, manage global app state with REDUX</Link>
+        </div>
+      </div>
     </MiddleDiv>
   )
 
