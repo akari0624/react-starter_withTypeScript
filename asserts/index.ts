cf53265e28a1ed8003e7b58a418d02cfc9b2a262
Morris-Chen <akari0624@hotmail.com>
2019-07-21 16:22:18 +0800

    增加assert的打包的設定（目前只有png, jpg兩種圖檔）

---
+++ asserts/index.ts
@@ -0,0 +1,3 @@
+import DKEffect from './Dunning-Kruger-Effect-and-CRO-1024x492.jpg'
+
+export { DKEffect }
--- src/mainpage/index.tsx
+++ src/mainpage/index.tsx
@@ -1,11 +1,7 @@
 import React from 'react'
 import Styled from 'styled-components'
+import { DKEffect } from '../../asserts'
 
-/** in Typescript, we need to write a interface(I feel more like a protocol) to the incoming props
- *  so tsc(TypeScript Compiler) can help us check the type in compile time
- *  https://stackoverflow.com/a/49280215/5599652
- *
- */
 interface Props {
   // onClick: Function;
 }
@@ -21,6 +17,7 @@ export default (props: Props) => {
   return (
     <MiddleDiv>
       <h3>{helloText}</h3>
+      <img src={DKEffect} />
     </MiddleDiv>
   )
 }
